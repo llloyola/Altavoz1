bebdfa1a75417c8e33d2d906c17da65d90098404
jaluco <jaluco@uc.cl>
2018-12-21 19:46:49 -0300

    Lucho pescame por la chucha

--- Tarea1-Mapas(NodeJS)/myapp/app.js
+++ Tarea1-Mapas(NodeJS)/myapp/app.js
@@ -7,7 +7,7 @@ var map = L.map('map', {
 });
 
 // Set the position and zoom level of the map
-map.setView([47.70, 13.35], 7);
+map.setView([-33.1, -71.7], 7);
 
 // Initialize the base layer
 var osm_mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -15,3 +15,48 @@ var osm_mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 	attribution: '&copy; OSM Mapnik <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+
+// Array de los marcadores
+var marcadores = [];
+
+const Http = new XMLHttpRequest();
+const url='http://localhost:9000/py$';
+Http.open("GET", url, true);
+Http.send();
+Http.onreadystatechange=(e)=>{
+	var texto = Http.responseText;
+	console.log(Http.responseText);
+	// console.log(map)
+
+	if (Http.readyState === 4) {
+	var data = JSON.parse(texto);
+	var buques = data.buques;
+	var aviones = data.aviones;
+
+
+	for (var i = buques.length - 1; i >= 0; i--) {
+		var marker = L.marker([buques[i].LAT, buques[i].LON]);
+		marker.addTo(map);
+
+		marcadores.push(marker);
+	}
+
+	setTimeout(function(){
+		marcadores.forEach(function (layer) {	
+	    map.removeLayer(layer);
+	    Http.open("GET", url, true);
+		Http.send();
+
+		});
+	}, 5000);
+//
+}
+				
+//				//var buques = data.buques;
+//				//var aviones = data.aviones;
+//
+//				//for (var i = buques.length - 1; i >= 0; i--) {
+//				//	
+//				//	L.marker([buques[i].LAT, buques[i].LON]).addTo(map);
+				//}
+			}
